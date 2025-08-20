9ecde8e9cab77a5775bfebb780096b3e86d1cf20
Azar m <azar.mohammed@cittabase.com>
2025-08-20 02:36:51 +0000

    api-integration

--- Create SQL Statement.sql
+++ Create SQL Statement.sql
@@ -1,2 +1,7 @@
 CREATE DATABASE SNOWPARK_ML_DEMO;
 
+create or replace api integration git_api_integration
+    api_provider = git_https_api
+    API_ALLOWED_PREFIXES = ('https://github.com/azar-c')
+    enabled = true
+    allowed_authentication_secrets = all;
